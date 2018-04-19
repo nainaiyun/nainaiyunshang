f5995c354200ad96965b092c514782904ef4656e
yanhaopeng <1071477992@qq.com>
2018-04-19 10:35:34 +0800

    认证api

--- nainaiwang/user/src/main/java/com/nainai/user/controller/AuthenticationController.java
+++ nainaiwang/user/src/main/java/com/nainai/user/controller/AuthenticationController.java
@@ -1,5 +1,17 @@
 package com.nainai.user.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.nainai.user.common.Result;
+import com.nainai.user.common.ResultGenerator;
+import com.nainai.user.domain.User;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by haopeng yan on 2018/4/16
  *
@@ -8,5 +20,26 @@ package com.nainai.user.controller;
  * @since 1.0
  * Copyright (C) 2018. haopeng yan All Rights Received
  */
+@RestController
+@RequestMapping("/authentication")
 public class AuthenticationController {
+    @ApiOperation(value = "企业认证", notes = "企业认证")
+    @ApiImplicitParams({
+            @ApiImplicitParam(dataType = "String", name = "username", value = "用户名")
+    })
+    @RequestMapping(value="/company",method = RequestMethod.POST)
+    public Result test(@RequestBody JSONObject jsonObject) {
+        String username = jsonObject.getString("username");
+        return ResultGenerator.genSuccessResult(username);
+    }
+
+    @ApiOperation(value = "个人认证", notes = "个人认证")
+    @ApiImplicitParams({
+            @ApiImplicitParam(dataType = "String", name = "username", value = "用户名")
+    })
+    @RequestMapping(value="/personal",method = RequestMethod.POST)
+    public Result personal(@RequestBody JSONObject jsonObject) {
+        String username = jsonObject.getString("username");
+        return ResultGenerator.genSuccessResult(username);
+    }
 }
