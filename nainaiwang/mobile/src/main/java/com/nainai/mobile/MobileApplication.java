f293461f4610874965b7f7d3c5e3997dd69dba1d
yanhaopeng <34259895+yanhaopeng@users.noreply.github.com>
2018-04-20 14:34:58 +0800

    默认的

--- nainaiwang/mobile/src/main/java/com/nainai/mobile/MobileApplication.java
+++ nainaiwang/mobile/src/main/java/com/nainai/mobile/MobileApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MobileApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MobileApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MobileApplication.class, args);
+    }
 }
