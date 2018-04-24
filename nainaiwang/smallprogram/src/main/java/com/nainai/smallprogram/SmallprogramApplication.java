d762e97069b27f4495afa62b9504390ca1a59bb7
yanhaopeng <1071477992@qq.com>
2018-04-24 16:21:27 +0800

    新增小程序api模块

---
+++ nainaiwang/smallprogram/src/main/java/com/nainai/smallprogram/SmallprogramApplication.java
@@ -0,0 +1,12 @@
+package com.nainai.smallprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallprogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallprogramApplication.class, args);
+	}
+}
